50d32f033205e839c8ea65faa09c7f31b9e4a5d4
Mohanee <mohanee99@gmail.com>
2020-10-05 14:11:06 +0530

    [mohanee] add create board program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace TicTacToeGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Welcome to TictacToe Game");
+            TicTacToe t = new TicTacToe();
+            t.CreateBoard();
         }
     }
+
+    class TicTacToe
+    {
+        public void CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 1; i < 10; i++)
+            {
+                board[i] = Convert.ToChar(0);
+            }
+        }
+    }
+
+
+
 }
