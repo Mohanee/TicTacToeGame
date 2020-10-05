1ff86aa45d5c6a6091f52a57c2edaaa423a0067b
Mohanee <mohanee99@gmail.com>
2020-10-05 15:02:28 +0530

    [mohanee] add choose letter method

--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ namespace TicTacToeGame
             Console.WriteLine("Hello Welcome to TictacToe Game");
             TicTacToe t = new TicTacToe();
             t.CreateBoard();
+            char pLetter = t.ChooseLetter();
+            char cLetter='X';
+            if(pLetter.Equals('X'))
+            {
+                cLetter = 'O';
+            }
         }
     }
 
@@ -21,6 +27,14 @@ namespace TicTacToeGame
             {
                 board[i] = Convert.ToChar(0);
             }
+        }
+
+        public char ChooseLetter()
+        {
+            Console.WriteLine("Choose a letter among X and O");
+            char pLetter = Convert.ToChar(Console.Read());
+
+            return pLetter;
         }
     }
 
